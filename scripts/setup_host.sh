128f1bdc8cc31f930bc779f22cfd398ab11d346b
lyeh <lyeh@devops.lyeh.virtualbox.org>
2025-07-09 12:15:47 +0200

    feat: Install the virtualbox in the host

--- scripts/setup_host.sh
+++ scripts/setup_host.sh
@@ -30,3 +30,12 @@ rm "/tmp/${DEB_FILE}"
 echo "✅ Vagrant installed successfully:"
 vagrant --version
 
+echo "Installing VirtualBox..."
+wget -O- -q https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmour -o /usr/share/keyrings/oracle_vbox_2016.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle_vbox_2016.gpg] http://download.virtualbox.org/virtualbox/debian bookworm contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+sudo apt update
+sudo apt install virtualbox-7.1
+
+# Verify VirtualBox installation
+echo "✅ VirtualBox installed successfully:"
+virtualbox --help | head -n 1
