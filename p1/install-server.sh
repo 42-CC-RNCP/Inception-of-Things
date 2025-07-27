e624cd0c33eaebd061dcfdb306eae91efd89b8b7
nweghofe <nweghofe@Debian.myguest.virtualbox.org>
2025-07-27 15:37:48 +0200

    feat: kubectl available for vagrant user

--- p1/install-server.sh
+++ p1/install-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --tls-san 192.168.56.110 --write-kubeconfig-mode 644"  sh -
 
-cat /var/lib/rancher/k3s/server/node-token > /vagrant_data/secrets
+install -m 600 /var/lib/rancher/k3s/server/node-token /vagrant_data/secrets
