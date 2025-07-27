9d3c6b5e1e4dd98f7e82d100d5528a726d16b384
yehya <lea.yeh.ml@gmail.com>
2025-07-27 16:14:59 +0200

    Revert "refactor: restructure folder structure"
    This reverts commit 90ba06674c9ec33f41fc2aa6f90429ebd9dd6fb1.

--- p1/tools/install-agent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SERVER_IP=$1
-
-export K3S_TOKEN=$(cat /vagrant_data/secrets)
-export K3S_URL="https://${SERVER_IP}:6443"
-
-curl -sfL https://get.k3s.io | \
-    INSTALL_K3S_EXEC="agent" K3S_URL=${K3S_URL} K3S_TOKEN=${K3S_TOKEN} sh -
