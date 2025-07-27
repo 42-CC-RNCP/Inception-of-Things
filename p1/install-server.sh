5c67cab0b3f2c685260d27522bcb0bb1d41f915a
yehya <lea.yeh.ml@gmail.com>
2025-07-27 16:14:59 +0200

    Revert "feat: kubectl available for vagrant user"
    This reverts commit e624cd0c33eaebd061dcfdb306eae91efd89b8b7.

--- p1/install-server.sh
+++ p1/install-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --tls-san 192.168.56.110 --write-kubeconfig-mode 644"  sh -
+curl -sfL https://get.k3s.io | sh -
 
-install -m 600 /var/lib/rancher/k3s/server/node-token /vagrant_data/secrets
+cat /var/lib/rancher/k3s/server/node-token > /vagrant_data/secrets
