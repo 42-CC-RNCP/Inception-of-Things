2fdf08c4a3a16ac1959ca0a3a511f9678a334b8b
nweghofe <nweghofe@Debian.myguest.virtualbox.org>
2025-07-27 15:37:48 +0200

    feat: kubectl available for vagrant user

--- p1/install-agent.sh
+++ p1/install-agent.sh
@@ -3,4 +3,4 @@
 export K3S_TOKEN=$(cat /vagrant_data/secrets)
 export K3S_URL='https://192.168.56.110:6443'
 
-curl -sfL https://get.k3s.io | K3S_URL=${K3S_URL} K3S_TOKEN=${K3S_TOKEN} sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent" K3S_URL=${K3S_URL} K3S_TOKEN=${K3S_TOKEN} sh -
