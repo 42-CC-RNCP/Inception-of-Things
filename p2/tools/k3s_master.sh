e212d6b2f73f869e8d5f2fa4903f7ba37d9fb367
vhappenh <vhappenhofer@live.at>
2025-08-03 16:40:25 +0200

    refactor: Updates structure and advances readability by implementing kustomization.yaml.

--- p2/tools/k3s_master.sh
+++ p2/tools/k3s_master.sh
@@ -6,14 +6,4 @@ SERVER_IP=$1
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server \
     --tls-san ${SERVER_IP} --write-kubeconfig-mode 644" sh -
 
-kubectl apply -f /vagrant/k8s/app1.yaml
-kubectl apply -f /vagrant/k8s/app1-service.yaml
-kubectl apply -f /vagrant/k8s/app2.yaml
-kubectl apply -f /vagrant/k8s/app2-service.yaml
-kubectl apply -f /vagrant/k8s/app3.yaml
-kubectl apply -f /vagrant/k8s/app3-service.yaml
-
-kubectl create configmap app1-html --from-file=/vagrant/tools/app1.html
-kubectl create configmap app2-html --from-file=/vagrant/tools/app2.html
-kubectl create configmap app3-html --from-file=/vagrant/tools/app3.html
-kubectl apply -f /vagrant/k8s/ingress.yaml
+kubectl apply -k /vagrant/k8s
