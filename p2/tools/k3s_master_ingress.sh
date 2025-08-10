3a6619f5f1c8702929faa1150ddb706dbeb1de5a
vhappenh <vhappenhofer@live.at>
2025-08-10 15:43:37 +0200

    feat: fixes refactoring bugs.

--- p2/tools/k3s_master_ingress.sh
+++ p2/tools/k3s_master_ingress.sh
@@ -6,4 +6,4 @@ SERVER_IP=$1
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server \
     --tls-san ${SERVER_IP} --write-kubeconfig-mode 644" sh -
 
-kubectl apply -k /vagrant/k8s
+kubectl apply -k /vagrant/manifests/overlays/ingress
